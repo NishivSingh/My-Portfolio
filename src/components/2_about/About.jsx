ca07581d98a6288ce3a0e39a955c119f75149a47
Nishiv Singh <singh.136@iitj.ac.in>
2023-07-16 21:18:24 +0530

    Added about section

--- src/components/2_about/About.jsx
+++ src/components/2_about/About.jsx
@@ -1,8 +1,38 @@
 import React from "react";
 import "./about.css";
+import about_me from "..//../assets/images/my_image.png";
 
 const About = () => {
-  return <section id="about">About</section>;
+  return (
+    <section id="about">
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about_container">
+        <div className="about_me">
+          <div className="about_me_image">
+            <img src={about_me} alt="#" />
+          </div>
+        </div>
+
+        <div className="about_content">
+          <div className="about_cards">
+            <article className="about_card">Card</article>
+          </div>
+          <div className="about_info">
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni,
+              magnam? Maiores numquam et ullam voluptatibus temporibus cum
+              cumque quia expedita porro adipisci. Sint in animi velit cumque?
+              Illo sit rem voluptates optio adipisci officia id deserunt nobis
+              blanditiis rerum. Sint voluptatem voluptatibus laborum quasi
+              architecto similique! Aliquam ipsam error impedit.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
