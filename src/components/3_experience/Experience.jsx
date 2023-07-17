23aed11a26a968ce77b09078bfffef299aae7a97
Nishiv Singh <singh.136@iitj.ac.in>
2023-07-17 16:04:11 +0530

    Added experience section in the app

--- src/components/3_experience/Experience.jsx
+++ src/components/3_experience/Experience.jsx
@@ -1,8 +1,53 @@
 import React from "react";
 import "./experience.css";
 
+function SkillCard({ class_name, title, skills }) {
+  const combinedClass = `skill_card ${class_name}`;
+
+  return (
+    <div className={combinedClass}>
+      <h3>{title}</h3>
+      <div className="experience_content">
+        {skills.map((skill, index) => (
+          <article className="experience_details">
+            <h4>{skill}</h4>
+          </article>
+        ))}
+      </div>
+    </div>
+  );
+}
+
 const Experience = () => {
-  return <section id="experience">Experience</section>;
+  return (
+    <section id="experience">
+      <h5>What Skill I have</h5>
+      <h2>My Experience</h2>
+
+      <div className="container experience_container">
+        <SkillCard
+          class_name={"programming"}
+          title={"Programming"}
+          skills={["C++", "Python", "Javascript"]}
+        />
+        <SkillCard
+          class_name={"web_dev"}
+          title={"Web Development"}
+          skills={["HTML", "CSS", "Javascript", "React"]}
+        />
+        <SkillCard
+          class_name={"app_dev"}
+          title={"App Development"}
+          skills={["Flutter", "Firebase"]}
+        />
+        <SkillCard
+          class_name={"others"}
+          title={"Others"}
+          skills={["Git", "ML", "Matlab"]}
+        />
+      </div>
+    </section>
+  );
 };
 
 export default Experience;
