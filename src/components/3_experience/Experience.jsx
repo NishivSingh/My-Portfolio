e98d24ecbe282ffc2f0f71ec7b0c4b3485f1c9ae
Nishiv Singh <singh.136@iitj.ac.in>
2023-08-01 20:01:12 +0530

    Added skill icons and changed some styling

--- src/components/3_experience/Experience.jsx
+++ src/components/3_experience/Experience.jsx
@@ -8,8 +8,9 @@ function SkillCard({ class_name, title, skills }) {
     <div className={combinedClass}>
       <h3>{title}</h3>
       <div className="experience_content">
-        {skills.map((skill, index) => (
+        {Object.entries(skills).map(([skill, links], index) => (
           <article key={index} className="experience_details">
+            <img width="240" height="240" src={links[0]} alt={links[1]} className={links[1]==="nextjs"?"nextjs":"images"} />
             <h4>{skill}</h4>
           </article>
         ))}
@@ -26,32 +27,44 @@ const Experience = () => {
 
       <div className="container experience_container">
         <SkillCard
-          class_name={"web_dev"}
-          title={"Web Development"}
-          skills={[
-            "HTML",
-            "CSS",
-            "Tailwind CSS",
-            "Javascript",
-            "Typescript",
-            "React JS",
-            "Next JS",
-          ]}
+          class_name={"frontend_dev"}
+          title={"Frontend Development"}
+          skills={{
+            "HTML": [
+              "https://img.icons8.com/color/240/html-5--v1.png",
+              "html-5",
+            ],
+            "CSS": ["https://img.icons8.com/color/240/css3.png", "css3"],
+            "Tailwind CSS": [
+              "https://img.icons8.com/color/240/tailwind_css.png",
+              "tailwind_css",
+            ],
+            "Javascript": ["https://img.icons8.com/color/240/javascript--v1.png","javascript"],
+            "Typescript": ["https://img.icons8.com/color/240/typescript.png","typescript"],
+            "React Js": ["https://img.icons8.com/plasticine/200/react.png","react"],
+            "Next Js": ["https://cdn.worldvectorlogo.com/logos/nextjs-2.svg","nextjs"],
+          }}
         />
         <SkillCard
           class_name={"app_dev"}
           title={"Mobile App Development"}
-          skills={["Flutter", "Firebase"]}
-        />
-        <SkillCard
-          class_name={"ml"}
-          title={"Machine Learning"}
-          skills={["Python", "Streamlit", "Pandas", "Numpy", "Scikit-learn"]}
+          skills={{
+            "Dart": ["https://img.icons8.com/color/240/dart.png","dart"],
+            "Flutter": ["https://img.icons8.com/color/240/flutter.png","flutter"],
+            "Firebase": ["https://img.icons8.com/color/240/firebase.png","firebase"],
+            "Figma": ["https://img.icons8.com/color/480/figma--v1.png","figma"],
+          }}
         />
         <SkillCard
           class_name={"others"}
           title={"Others"}
-          skills={["C++", "Matlab", "Git/Github", "Linux"]}
+          skills={{
+            "C": ["https://raw.githubusercontent.com/devicons/devicon/master/icons/c/c-original.svg","C"],
+            "C++": ["https://img.icons8.com/fluency/240/c-plus-plus-logo.png","c++"],
+            "Python": ["https://img.icons8.com/color/240/python--v1.png","python"],
+            "Linux": ["https://img.icons8.com/color/240/linux--v1.png" ,"linux"],
+            "Matlab": ["https://img.icons8.com/fluency/240/matlab.png","matlab"],
+          }}
         />
       </div>
     </section>
