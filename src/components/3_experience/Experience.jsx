e92503ac028410baf42a98bf7ded104ae6e5329e
Nishiv Singh <singh.136@iitj.ac.in>
2023-08-20 23:51:15 +0530

    changes in skill section

--- src/components/3_experience/Experience.jsx
+++ src/components/3_experience/Experience.jsx
@@ -10,7 +10,7 @@ function SkillCard({ class_name, title, skills }) {
       <div className="experience_content">
         {Object.entries(skills).map(([skill, links], index) => (
           <article key={index} className="experience_details">
-            <img width="240" height="240" src={links[0]} alt={links[1]} className={links[1]==="nextjs"?"nextjs":"images"} />
+            <img width="240" height="240" src={links[0]} alt={links[1]} className={links[1] === "nextjs" ? "nextjs" : "images"} />
             <h4>{skill}</h4>
           </article>
         ))}
@@ -39,34 +39,34 @@ const Experience = () => {
               "https://img.icons8.com/color/240/tailwind_css.png",
               "tailwind_css",
             ],
-            "Javascript": ["https://img.icons8.com/color/240/javascript--v1.png","javascript"],
-            "Typescript": ["https://img.icons8.com/color/240/typescript.png","typescript"],
-            "React Js": ["https://img.icons8.com/plasticine/200/react.png","react"],
-            "Next Js": ["https://cdn.worldvectorlogo.com/logos/nextjs-2.svg","nextjs"],
+            "Javascript": ["https://img.icons8.com/color/240/javascript--v1.png", "javascript"],
+            "Typescript": ["https://img.icons8.com/color/240/typescript.png", "typescript"],
+            "React Js": ["https://img.icons8.com/plasticine/200/react.png", "react"],
+            "Next Js": ["https://cdn.worldvectorlogo.com/logos/nextjs-2.svg", "nextjs"],
           }}
         />
         <div className="non_web_dev">
-        <SkillCard
-          class_name={"app_dev"}
-          title={"Mobile App Development"}
-          skills={{
-            "Dart": ["https://img.icons8.com/color/240/dart.png","dart"],
-            "Flutter": ["https://img.icons8.com/color/240/flutter.png","flutter"],
-            "Firebase": ["https://img.icons8.com/color/240/firebase.png","firebase"],
-            "Figma": ["https://img.icons8.com/color/480/figma--v1.png","figma"],
-          }}
-        />
-        <SkillCard
-          class_name={"others"}
-          title={"Others"}
-          skills={{
-            "C": ["https://raw.githubusercontent.com/devicons/devicon/master/icons/c/c-original.svg","C"],
-            "C++": ["https://img.icons8.com/fluency/240/c-plus-plus-logo.png","c++"],
-            "Python": ["https://img.icons8.com/color/240/python--v1.png","python"],
-            "Linux": ["https://img.icons8.com/color/240/linux--v1.png" ,"linux"],
-            "Matlab": ["https://img.icons8.com/fluency/240/matlab.png","matlab"],
-          }}
-        />
+          <SkillCard
+            class_name={"app_dev"}
+            title={"Mobile App Development"}
+            skills={{
+              "Dart": ["https://img.icons8.com/color/240/dart.png", "dart"],
+              "Flutter": ["https://img.icons8.com/color/240/flutter.png", "flutter"],
+              "Firebase": ["https://img.icons8.com/color/240/firebase.png", "firebase"],
+            }}
+          />
+          <SkillCard
+            class_name={"others"}
+            title={"Others"}
+            skills={{
+              "C": ["https://raw.githubusercontent.com/devicons/devicon/master/icons/c/c-original.svg", "C"],
+              "C++": ["https://img.icons8.com/fluency/240/c-plus-plus-logo.png", "c++"],
+              "Python": ["https://img.icons8.com/color/240/python--v1.png", "python"],
+              "Figma": ["https://img.icons8.com/color/480/figma--v1.png", "figma"],
+              "Linux": ["https://img.icons8.com/color/240/linux--v1.png", "linux"],
+              "Matlab": ["https://img.icons8.com/fluency/240/matlab.png", "matlab"],
+            }}
+          />
         </div>
       </div>
     </section>
