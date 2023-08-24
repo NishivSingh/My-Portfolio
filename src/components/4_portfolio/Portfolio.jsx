b35dba941776579f1544f17418c4b97c65913225
Nishiv Singh <singh.136@iitj.ac.in>
2023-08-24 15:34:44 +0530

    Creating a separate file for fetching data

--- src/components/4_portfolio/Portfolio.jsx
+++ src/components/4_portfolio/Portfolio.jsx
@@ -1,11 +1,6 @@
 import React from "react";
 import "./portfolio.css";
-import shop_sphere from "..//..//assets/images/portfolio/shop_sphere.png";
-import ai_image_generator from "..//../assets/images/portfolio/ai-image-generator.png";
-import chat_app from "..//..//assets/images/portfolio/chat-app.png";
-import sorting_vis from "..//..//assets/images/portfolio/sorting-visualizer.png";
-import movie_info from "..//..//assets/images/portfolio/movie-Info.png";
-import youtube_bookmarker from "..//..//assets/images/portfolio/youtube-bookmarker.png";
+import portfolioItems from "./portfolioData";
 
 function ProjectCard({ img, gitlink, weblink, title }) {
   return (
@@ -24,43 +19,17 @@ const Portfolio = () => {
       <h2>Portfolio</h2>
 
       <div className="container portfolio_container">
-        <ProjectCard
-          img={shop_sphere}
-          title={"ShopSphere"}
-          gitlink={"https://github.com/NishivSingh/restaurant-website"}
-          weblink={"http://github.com"}
-        />
-        <ProjectCard
-          img={ai_image_generator}
-          title={"AI-Image-Generator"}
-          gitlink={"https://github.com/NishivSingh/AI-Image-Generator"}
-          weblink={"http://github.com"}
-        />
-        <ProjectCard
-          img={chat_app}
-          title={"Chit-Chat"}
-          gitlink={"https://github.com/NishivSingh/chat-app"}
-          weblink={"http://github.com"}
-        />
-        <ProjectCard
-          img={youtube_bookmarker}
-          title={"Youtube Bookmarker"}
-          gitlink={"https://github.com/NishivSingh/Multi-Calculator"}
-          weblink={"http://github.com"}
-        />
-        <ProjectCard
-          img={sorting_vis}
-          title={"Sorting Visualizer"}
-          gitlink={"https://github.com/NishivSingh/Sorting-Visualizer"}
-          weblink={"http://github.com"}
-        />
-        <ProjectCard
-          img={movie_info}
-          title={"Movies Info Website"}
-          gitlink={"https://github.com/NishivSingh/Movie-Info-App"}
-          weblink={"http://github.com"}
-        />
-
+        {
+          portfolioItems.map((item, index) => (
+            <ProjectCard
+              key={index}
+              img={item.img}
+              title={item.title}
+              gitlink={item.gitlink}
+              weblink={item.weblink}
+            />
+          ))
+        }
       </div>
     </section>
   );
---
+++ src/components/4_portfolio/portfolioData.js
@@ -0,0 +1,52 @@
+import shop_sphere from "..//..//assets/images/portfolio/shop_sphere.png";
+import ai_image_generator from "..//../assets/images/portfolio/ai-image-generator.png";
+import chat_app from "..//..//assets/images/portfolio/chat-app.png";
+import sorting_vis from "..//..//assets/images/portfolio/sorting-visualizer.png";
+import movie_info from "..//..//assets/images/portfolio/movie-Info.png";
+import youtube_bookmarker from "..//..//assets/images/portfolio/youtube-bookmarker.png";
+
+const portfolioItems = [
+  {
+    img: shop_sphere,
+    title: "ShopSphere",
+    gitlink: "https://github.com/NishivSingh/restaurant-website",
+    weblink: "http://github.com",
+  },
+
+  {
+    img: ai_image_generator,
+    title: "AI-Image-Generator",
+    gitlink: "https://github.com/NishivSingh/AI-Image-Generator",
+    weblink: "http://github.com",
+  },
+
+  {
+    img: chat_app,
+    title: "Chit-Chat",
+    gitlink: "https://github.com/NishivSingh/chat-app",
+    weblink: "http://github.com",
+  },
+
+  {
+    img: youtube_bookmarker,
+    title: "Youtube Bookmarker",
+    gitlink: "https://github.com/NishivSingh/Multi-Calculator",
+    weblink: "http://github.com",
+  },
+
+  {
+    img: sorting_vis,
+    title: "Sorting Visualizer",
+    gitlink: "https://github.com/NishivSingh/Sorting-Visualizer",
+    weblink: "http://github.com",
+  },
+
+  {
+    img: movie_info,
+    title: "Movies Info Website",
+    gitlink: "https://github.com/NishivSingh/Movie-Info-App",
+    weblink: "http://github.com",
+  },
+];
+
+export default portfolioItems;
