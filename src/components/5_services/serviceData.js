d155196b03804fc9ceb54aa6e6470f5a833a91a7
Nishiv Singh <singh.136@iitj.ac.in>
2023-10-01 23:15:28 +0530

    created a new file for efficient data management

---
+++ src/components/5_services/serviceData.js
@@ -0,0 +1,25 @@
+const serviceItems = [
+  {
+    title: "Web Development",
+    description: [
+      "Crafting websites that transcend code to deliver captivating digital experiences",
+      "Breathing life into web applications with the MERN stack, sculpting dynamic and standout solutions",
+      "Infusing artificial intelligence into your applications for enhanced brilliance and automated user experiences",
+      "Proficient in codebase management, adapt at integrating new features into existing systems",
+      "Ensuring seamless responsiveness across all devices, guaranteeing delightful experiences on every screen",
+    ],
+  },
+
+  {
+    title: "App Development",
+    description: [
+      "Translate concepts into digital realities on all screens, bringing your vision to life.",
+      "Effortlessly switch between iOS and Android through expert cross-platform proficiency.",
+      "Foster real-time connections with instant messaging for heightened user engagement.",
+      "Focus on maximizing user engagement to maintain active user involvement.",
+      "Place a strong emphasis on trust, ensuring the security of user data through robust authentication methods.",
+    ],
+  },
+];
+
+export default serviceItems;
