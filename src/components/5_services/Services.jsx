b0dd608e7bed099d313cace849ba2c9acf052050
Nishiv Singh <singh.136@iitj.ac.in>
2023-10-01 23:16:16 +0530

    Created separate card for resusable component

--- src/components/5_services/Services.jsx
+++ src/components/5_services/Services.jsx
@@ -1,6 +1,27 @@
 import React from "react";
 import "./services.css";
 import { BiCheck } from "react-icons/bi";
+import serviceItems from "./serviceData";
+
+function ServiceCard({ item }) {
+  return (
+    <article className="service">
+      <div className="service_head">
+        <h3>{item.title}</h3>
+      </div>
+      <ul className="service_list">
+        {item.description.map((point, index) => (
+          <li key={index}>
+            <BiCheck className="service_list_icons" />
+            <p>
+              {point}
+            </p>
+          </li>
+        ))}
+      </ul>
+    </article>
+  )
+}
 const Services = () => {
   return (
     <section id="services">
@@ -8,82 +29,9 @@ const Services = () => {
       <h2>Services</h2>
 
       <div className="container services_container">
-        <article className="service">
-          <div className="service_head">
-            <h3>Web Development</h3>
-          </div>
-
-          <ul className="service_list">
-            <li>
-              <BiCheck className="service_list_icons" />
-              <p>
-                I create websites that go beyond code to deliver captivating digital experiences.
-              </p>
-            </li>
-            <li>
-              <BiCheck className="service_list_icons" />
-              <p>
-                I breathe life into web applications using the MERN stack, crafting standout, dynamic solutions.
-              </p>
-            </li>
-            <li>
-              <BiCheck className="service_list_icons" />
-              <p>
-                I infuse artificial intelligence into your apps, adding a touch of brilliance and automation to user experiences.
-              </p>
-            </li>
-            <li>
-              <BiCheck className="service_list_icons" />
-              <p>
-                Have a hand-on experience in codebase management, worked with existing code to include new features.
-              </p>
-            </li>
-            <li>
-              <BiCheck className="service_list_icons" />
-              <p>
-                Your website will be responsive on all devices, offering delightful experiences across screens.
-              </p>
-            </li>
-          </ul>
-        </article>
-        <article className="service">
-          <div className="service_head">
-            <h3>App Development</h3>
-          </div>
-
-          <ul className="service_list">
-            <li>
-              <BiCheck className="service_list_icons" />
-              <p>
-                My app development transforms ideas into digital realities, bringing your vision to life on every screen.
-              </p>
-            </li>
-            <li>
-              <BiCheck className="service_list_icons" />
-              <p>
-                Seamlessly navigate between iOS and Android with my cross-platform expertise.
-              </p>
-            </li>
-            <li>
-              <BiCheck className="service_list_icons" />
-              <p>
-                My mobile apps foster real-time connections, delivering instant messaging to boost engagement.
-              </p>
-            </li>
-            <li>
-              <BiCheck className="service_list_icons" />
-              <p>
-                I focus on maximizing user engagement to keep your app's audience actively involved and invested.
-              </p>
-            </li>
-            <li>
-              <BiCheck className="service_list_icons" />
-              <p>
-                My apps prioritize trust, implementing secure authentication for user data and interactions.
-              </p>
-            </li>
-          </ul>
-        </article>
+        {serviceItems.map((item, index) => (
+          <ServiceCard key={index} item={item} />
+        ))}
       </div>
     </section>
   );
